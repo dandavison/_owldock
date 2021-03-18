9a6d2240e6472ac228d25d733e8dcff9fe2228f2
Dan Davison <dandavison7@gmail.com>
2021-03-18 09:15:28 -0500

    Register models with admin

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Employee
+from .models import ImmigrationTask
+
+admin.site.register(Employee)
+admin.site.register(ImmigrationTask)
