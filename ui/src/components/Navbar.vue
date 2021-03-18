a1098f1c9db44b905f947d3606fceaa44b7802e3
Dan Davison <dandavison7@gmail.com>
2021-03-18 08:19:13 -0500

    Cleaner navbar appearance

--- ui/src/components/Navbar.vue
+++ ui/src/components/Navbar.vue
@@ -2,16 +2,19 @@
   <b-navbar>
     <template slot="brand">
       <b-navbar-item>
-        <i class="fas fa-globe-americas mr-2"></i>
-        <router-link to="/"> <b>Global Mobility Dashboard</b> </router-link>
+        <router-link to="/">
+          <i class="fas fa-globe-americas"></i>
+        </router-link>
       </b-navbar-item>
+
+      <b-navbar-dropdown label="Global Mobility Dashboard" arrowless>
+        <b-navbar-item>
+          <router-link to="/"> Help </router-link>
+        </b-navbar-item>
+      </b-navbar-dropdown>
     </template>
 
     <template slot="end">
-      <b-navbar-item>
-        <router-link to="/"> Help </router-link>
-      </b-navbar-item>
-
       <b-navbar-dropdown :label="loggedInUserName() || 'Account'" arrowless>
         <b-navbar-item>
           <a :href="logoutURL"> Log out </a>
