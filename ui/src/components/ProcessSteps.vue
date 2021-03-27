a5607d18a2c7fc706ef5fc0d67090d8a445a4722
Dan Davison <dandavison7@gmail.com>
2021-03-27 15:25:27 -0400

    Tweak Process component: flatter and more compact

--- ui/src/components/ProcessSteps.vue
+++ ui/src/components/ProcessSteps.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="card">
-    <div class="card-content">
-      <h2 class="subtitle">Steps</h2>
-      <ol class="ml-6">
-        <li v-for="step of steps" :key="step.sequence_number">
-          {{ step.service.name }}
-        </li>
-      </ol>
-    </div>
+  <div>
+    <h3 class="subtitle is-5 mb-2">Steps</h3>
+    <ol class="ml-6">
+      <li v-for="step of steps" :key="step.sequence_number">
+        {{ step.service.name }}
+      </li>
+    </ol>
   </div>
 </template>
 
