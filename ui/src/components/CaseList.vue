7135035d9fb8101538c77d616fa69d6e44372a8f
Dan Davison <dandavison7@gmail.com>
2021-03-31 21:21:01 -0400

    Revert back to one-card-per-row table

--- ui/src/components/CaseList.vue
+++ ui/src/components/CaseList.vue
@@ -1,92 +1,38 @@
 <template>
-  <b-tabs>
-    <b-tab-item label="Table">
-      <b-table
-        ref="table"
-        :data="rows"
-        :selected.sync="selected"
-        focusable
-        hoverable
-        paginated
-        @dblclick="navigateToRowDetailView"
-        :per-page="50"
-      >
-        <b-table-column
-          field="applicantNameData"
-          label="Applicant"
-          v-slot="props"
+  <!-- class="section" on the following div makes the table rows vertically aligned -->
+  <div>
+    <b-tabs>
+      <b-tab-item label="Table">
+        <b-table
+          ref="table"
+          :data="rows"
+          :selected.sync="selected"
+          focusable
+          hoverable
+          paginated
+          @dblclick="navigateToRowDetailView"
+          :per-page="10"
         >
-          {{ props.row.applicantNameDisplay }}
-        </b-table-column>
+          <b-table-column v-slot="props">
+            <case
+              style="width: 100%"
+              :case_="props.row"
+              :showSteps="false"
+            ></case>
+          </b-table-column>
+        </b-table>
+      </b-tab-item>
 
-        <b-table-column
-          field="applicantNationalitiesData"
-          label="Nationalities"
-          v-slot="props"
-        >
-          {{ props.row.applicantNationalitiesDisplay }}
-        </b-table-column>
-
-        <b-table-column
-          field="process.route.host_country.name"
-          label="Host country"
-          v-slot="props"
-        >
-          {{ props.row.process.route.host_country.unicode_flag }}
-        </b-table-column>
-
-        <b-table-column
-          v-if="role !== Role.ProviderContact"
-          field="providerContactName"
-          label="Provider Contact"
-          v-slot="props"
-        >
-          {{ props.row.providerContactName }}
-        </b-table-column>
-
-        <b-table-column
-          v-if="role !== Role.ProviderContact"
-          label="Provider"
-          v-slot="props"
-        >
-          {{ props.row.provider_contact.provider.name }}
-        </b-table-column>
-
-        <b-table-column label="Process" v-slot="props">
-          {{ props.row.process.route.name }}
-        </b-table-column>
-
-        <b-table-column
-          field="dateInitiated"
-          label="Date initiated"
-          v-slot="props"
-        >
-          {{ new Date(props.row.created_at).toDateString() }}
-        </b-table-column>
-
-        <b-table-column
-          field="targetDate"
-          label="Target entry date"
-          v-slot="props"
-        >
-          {{ new Date(props.row.target_entry_date).toDateString() }}
-        </b-table-column>
-
-        <b-table-column field="progress" label="Progress" v-slot="props">
-          0 / {{ props.row.process.steps.length }}
-        </b-table-column>
-      </b-table>
-    </b-tab-item>
-
-    <b-tab-item label="Selected">
-      <ul>
-        <li>Milestones completed</li>
-        <li>Documents</li>
-        <li>Exchange documents</li>
-        <li>Send notification / message to provider</li>
-      </ul>
-    </b-tab-item>
-  </b-tabs>
+      <b-tab-item label="Selected">
+        <ul>
+          <li>Milestones completed</li>
+          <li>Documents</li>
+          <li>Exchange documents</li>
+          <li>Send notification / message to provider</li>
+        </ul>
+      </b-tab-item>
+    </b-tabs>
+  </div>
 </template>
 
 <script lang="ts">
@@ -96,16 +42,15 @@ type BTableInstance = InstanceType<typeof BTable>;
 
 import { Role } from "../role";
 import { CaseSerializer } from "../api-types";
-import { applicantUnicodeFlags } from "@/methods";
+import Case from "../components/Case.vue";
 
 export default Vue.extend({
   props: { role: Number as PropType<Role> },
-
+  components: { Case },
   data() {
     return {
       rows: [] as CaseSerializer[],
       selected: {},
-      Role,
     };
   },
 
@@ -137,23 +82,11 @@ export default Vue.extend({
           return;
       }
       const resp = await fetch(url);
-      const data = await resp.json();
-      this.rows = data.map(this.transformRow);
+      this.rows = await resp.json();
     },
 
     navigateToRowDetailView(row: CaseSerializer): void {
       this.$router.push(`/portal/case/${row.id}`);
-    },
-
-    transformRow(row: CaseSerializer): CaseSerializer {
-      const applicantName = `${row.applicant.user.first_name} ${row.applicant.user.last_name}`;
-      return Object.assign(row, {
-        applicantNameData: applicantName,
-        applicantNameDisplay: applicantName,
-        applicantNationalitiesData: row.applicant.nationalities.join(", "),
-        applicantNationalitiesDisplay: applicantUnicodeFlags(row.applicant),
-        providerContactName: `${row.provider_contact.user.first_name} ${row.provider_contact.user.last_name}`,
-      });
     },
   },
 });
