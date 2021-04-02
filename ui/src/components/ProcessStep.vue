0dcf3b34a072c703ba343f4aee9ca1fd5a459d00
Dan Davison <dandavison7@gmail.com>
2021-04-01 21:40:06 -0400

    Evolve process steps component

---
+++ ui/src/components/ProcessStep.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>
+      <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">
+            {{ step.sequence_number }}. {{ step.service.name }}
+          </p>
+          <button class="card-header-icon" @click="showDetail = !showDetail">
+            <span class="icon">
+              <i class="fas fa-angle-down"></i>
+            </span>
+          </button>
+        </header>
+        <div v-if="showDetail" class="card-content">
+          <div class="content">
+            [Additional information for step {{ step.sequence_number }}]
+          </div>
+        </div>
+        <footer v-if="showDetail" class="card-footer">
+          <a href="#" class="card-footer-item">
+            <i class="fas fa-upload"></i> <span class="ml-2">Upload file</span>
+          </a>
+        </footer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+
+import { ProcessStepSerializer } from "../api-types";
+
+export default Vue.extend({
+  props: { step: Object as PropType<ProcessStepSerializer> },
+
+  data() {
+    return {
+      showDetail: false,
+    };
+  },
+});
+</script>
+
+<style scoped>
+</style>
--- ui/src/components/ProcessSteps.vue
+++ ui/src/components/ProcessSteps.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
     <h3 class="subtitle is-5 mb-2">Steps</h3>
-    <ol class="ml-6">
+    <ul>
       <li v-for="step of steps" :key="step.sequence_number">
-        {{ step.service.name }}
+        <process-step :step="step"></process-step>
       </li>
-    </ol>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import Vue, { PropType } from "vue";
 
+import ProcessStep from "./ProcessStep.vue";
 import { ProcessStepSerializer } from "../api-types";
 
 export default Vue.extend({
+  components: { ProcessStep },
+
   props: { steps: Array as PropType<ProcessStepSerializer[]> },
 });
 </script>
