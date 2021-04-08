2ce49bbcf2c58d40b451d40ea9df66f39bdddc56
Dan Davison <dandavison7@gmail.com>
2021-04-08 18:23:02 -0400

    Revert install django-fsm-log
    Partial revert of abdad85813e866defe39e84785a83657f711001a.
    
    Without this, tests fail with `no such table: django_content_type`.
    django-fsm-log uses the contentypes framework and I don't yet know
    whether that's possible in the client database; currently the
    django_content_type table is in the 'default' db only.

--- backend/owldock/settings/__init__.py
+++ backend/owldock/settings/__init__.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_countries",
-    "django_fsm_log",
     "corsheaders",
     "rest_framework",
     "app",
