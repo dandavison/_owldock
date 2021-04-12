0527ef918e01f226080c4ca5fd3159ce25c24bcf
Dan Davison <dandavison7@gmail.com>
2021-04-11 23:37:21 -0400

    Set DEFAULT_AUTO_FIELD

--- backend/owldock/settings/__init__.py
+++ backend/owldock/settings/__init__.py
@@ -140,5 +140,6 @@ STATIC_ROOT = "static"
 # App-specific settings
 
 CORS_ALLOW_ALL_ORIGINS = True
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
