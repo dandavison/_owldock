fda8ff88b628a0d99194c32e7649b62c283e17db
Dan Davison <dandavison7@gmail.com>
2021-04-15 19:49:25 -0400

    Use delta to diff database changes across transition

--- backend/app/signal_receivers.py
+++ backend/app/signal_receivers.py
@@ -1,12 +1,12 @@
-import json
 from pprint import pprint
 from threading import local
 
 from clint.textui import colored
-from deepdiff import DeepDiff
 from django.dispatch import receiver
 from django_fsm.signals import post_transition
 from django_fsm.signals import pre_transition
+
+from owldock.dev.diff_utils import print_diff
 
 THREAD_LOCALS = local()
 
@@ -33,10 +33,4 @@ def handle_django_fsm_post_transition(sender, **kwargs):
     before = getattr(THREAD_LOCALS, "pre_transition_data", None)
     after = getattr(THREAD_LOCALS, "post_transition_data", None)
     if before and after:
-        print_delta(before, after)
-
-
-def print_delta(before: dict, after: dict) -> None:
-    diff = DeepDiff(before, after)
-    print(colored.black("Δ", bold=True))
-    print(diff)
+        print_diff(before, after)
