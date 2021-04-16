b4b2e3296c78dcd271ca0723cdd1f4052b4082c6
Dan Davison <dandavison7@gmail.com>
2021-04-16 13:36:44 -0400

    Evolve delta diff utility

--- backend/owldock/dev/diff_utils.py
+++ backend/owldock/dev/diff_utils.py
@@ -1,26 +1,20 @@
 import json
-import os
 
 from difflib import unified_diff
 from subprocess import Popen, PIPE
 
 
-def to_json(obj):
-    return json.dumps(obj, sort_keys=True, indent=2)
-
-
-def print_diff(a, b, formatter=to_json):
+def print_diff(a, b):
     """
     https://github.com/dandavison/delta
     """
     if not (isinstance(a, str) and isinstance(b, str)):
-        a = formatter(a)
-        b = formatter(b)
+        a = json.dumps(a, sort_keys=True, indent=2)
+        b = json.dumps(b, sort_keys=True, indent=2)
     diff = "\n".join(unified_diff(a.splitlines(), b.splitlines()))
     if not diff:
         print("<no change>")
     else:
-        os.environ["BAT_PAGER"] = "cat"
-        proc = Popen(["delta"], stdin=PIPE)
+        proc = Popen(["delta", "--paging", "never"], stdin=PIPE)
         proc.stdin.write(diff.encode("utf-8"))  # type: ignore
         proc.communicate()
