47d9fb49d5071db41b4122eb39de70c2b47e707b
Dan Davison <dandavison7@gmail.com>
2021-04-19 18:57:17 -0400

    Evolve provider email

--- backend/app/outbound_messaging/__init__.py
+++ backend/app/outbound_messaging/__init__.py
@@ -15,20 +15,23 @@ class OfferedCaseStepsNotifier:
     case_steps: List[CaseStep]
 
     def notify(self):
-        [case] = [s.case for s in self.case_steps]
-        step_names = "- " + "\n- ".join(
-            s.process_step.service.name for s in self.case_steps
-        )
+        [case] = {s.case for s in self.case_steps}
+        client_contact_user = case.client_contact.user
+
         to_addresses = [self.provider_contact.user.email]
-        from_address = "owldock@owldock.com"
-        subject = "Owldock: your services have been requested"
+        from_address = "owl@owldock.com"
+        subject = "Your services have been requested"
         content = dedent(
             f"""
-            The following steps of case {case.uuid}
-            have been offered to you:
-            {step_names}
-
-            (TODO: Describe next actions provider should take)
+            Dear {self.provider_contact.provider.name} team,
+            
+            {client_contact_user.first_name} {client_contact_user.last_name}
+            (case.client_contact.client.name)
+            has selected you to assist with a service!
+            
+            Please log in to accept this work and learn the details
+            
+            https://owldock.com/portal/case/{case.uuid}
             """
         )
         send_mail(
@@ -40,8 +43,8 @@ class OfferedCaseStepsNotifier:
         )
         add_message(
             (
-                "Owldock does not actually send email yet.<br>"
-                "The following is what would be sent:"
+                "Owldock does not actually send notifications yet.<br>"
+                "Here is the email that would be sent:"
                 "<br>"
                 f"""
 <pre>
