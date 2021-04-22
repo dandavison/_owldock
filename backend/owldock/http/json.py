882bb06701a8da71e42debad3ca6ed3d34a9522d
Dan Davison <dandavison7@gmail.com>
2021-04-22 11:50:10 -0400

    Bug fix: do NOT use private _thread_locals variable from library

--- backend/owldock/http/json.py
+++ backend/owldock/http/json.py
@@ -1,24 +1,26 @@
 from typing import List
 
-from django.http import JsonResponse
-from django_tools.middlewares.ThreadLocal import _thread_locals
+from django.http import HttpRequest, JsonResponse
+from django_tools.middlewares.ThreadLocal import get_current_request
 
 _ERROR_MESSAGES_ATTR_NAME = "_error_messages_to_be_sent_with_response"
 _NON_ERROR_MESSAGES_ATTR_NAME = "_non_error_messages_to_be_sent_with_response"
 
 
 def add_error(error: str) -> None:
-    _extend(_ERROR_MESSAGES_ATTR_NAME, [error])
+    request = get_current_request()
+    _extend(request, _ERROR_MESSAGES_ATTR_NAME, [error])
 
 
 def add_message(message: str) -> None:
-    _extend(_NON_ERROR_MESSAGES_ATTR_NAME, [message])
+    request = get_current_request()
+    _extend(request, _NON_ERROR_MESSAGES_ATTR_NAME, [message])
 
 
-def _extend(attr_name: str, messages: List[str]) -> None:
-    if not hasattr(_thread_locals, attr_name):
-        setattr(_thread_locals, attr_name, [])
-    getattr(_thread_locals, attr_name).extend(messages)
+def _extend(request: HttpRequest, attr_name: str, messages: List[str]) -> None:
+    if not hasattr(request, attr_name):
+        setattr(request, attr_name, [])
+    getattr(request, attr_name).extend(messages)
 
 
 class OwldockJsonResponse(JsonResponse):
@@ -27,11 +29,12 @@ class OwldockJsonResponse(JsonResponse):
     """
 
     def __init__(self, data, errors=None, messages=None, **kwargs):
-        _extend(_ERROR_MESSAGES_ATTR_NAME, errors or [])
-        _extend(_NON_ERROR_MESSAGES_ATTR_NAME, messages or [])
+        request = get_current_request()
+        _extend(request, _ERROR_MESSAGES_ATTR_NAME, errors or [])
+        _extend(request, _NON_ERROR_MESSAGES_ATTR_NAME, messages or [])
 
-        errors = getattr(_thread_locals, _ERROR_MESSAGES_ATTR_NAME)
-        messages = getattr(_thread_locals, _NON_ERROR_MESSAGES_ATTR_NAME)
+        errors = getattr(request, _ERROR_MESSAGES_ATTR_NAME)
+        messages = getattr(request, _NON_ERROR_MESSAGES_ATTR_NAME)
 
         payload = {
             "data": data,
