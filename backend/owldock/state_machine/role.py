e344d8b0f2e4fd4a5ba95747b2656259f1b84470
Dan Davison <dandavison7@gmail.com>
2021-04-22 22:17:34 -0400

    Refactor: UserRole class wrapping client contact / provider contact

--- backend/owldock/state_machine/role.py
+++ backend/owldock/state_machine/role.py
@@ -1,6 +1,8 @@
 import logging
 from enum import Enum
-from typing import Optional
+from typing import Optional, Union
+
+from django.conf import settings
 
 logger = logging.getLogger(__file__)
 
@@ -10,21 +12,47 @@ class Role(Enum):
     PROVIDER_CONTACT = "Provider Contact"
 
 
-def get_role(user) -> Optional[Role]:
-    from client.models import ClientContact
-    from app.models import ProviderContact
+class UserRole:
+    def __init__(self, user: settings.AUTH_USER_MODEL):
+        from client.models import ClientContact
+        from app.models import ProviderContact
 
-    is_client_contact = ClientContact.objects.filter(user_uuid=user.uuid).exists()
-    is_provider_contact = ProviderContact.objects.filter(user=user).exists()
-    if is_client_contact and is_provider_contact:
-        logger.error(
-            "User %s is in both client-contact and provider-contact groups",
-            user,
-        )
-        return None
-    elif is_client_contact:
-        return Role.CLIENT_CONTACT
-    elif is_provider_contact:
-        return Role.PROVIDER_CONTACT
-    else:
-        return None
+        try:
+            self.client_contact = ClientContact.objects.get(user_uuid=user.uuid)
+        except ClientContact.DoesNotExist:
+            self.client_contact = None
+
+        try:
+            self.provider_contact = ProviderContact.objects.get(user=user)
+        except ProviderContact.DoesNotExist:
+            self.provider_contact = None
+
+        self._validate()
+
+    def _validate(self):
+        if self.client_contact and self.provider_contact:
+            logger.error(
+                "User %s is both a client contact and a provider contact",
+                self.user,
+            )
+
+    @property
+    def role(self) -> Role:
+        self._validate()
+        if self.client_contact:
+            return Role.CLIENT_CONTACT
+        if self.provider_contact:
+            return Role.PROVIDER_CONTACT
+        else:
+            return None
+
+    @property
+    def client_or_provider_contact(
+        self,
+    ) -> "Optional[Union[ClientContact, ProviderContact]]":
+        self._validate()
+        return self.client_contact or self.provider_contact or None
+
+
+def get_role(user) -> Optional[Role]:
+    return UserRole(user).role
