88a1db6b00d471690790571df905ea2601b111dd
Dan Davison <dandavison7@gmail.com>
2021-04-23 07:44:50 -0400

    Fix tests: don't assume there is a current HTTP request

--- backend/owldock/http/json.py
+++ backend/owldock/http/json.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from django.http import HttpRequest, JsonResponse
+from django.http import JsonResponse
 from django_tools.middlewares.ThreadLocal import get_current_request
 
 _ERROR_MESSAGES_ATTR_NAME = "_error_messages_to_be_sent_with_response"
@@ -8,19 +8,26 @@ _NON_ERROR_MESSAGES_ATTR_NAME = "_non_error_messages_to_be_sent_with_response"
 
 
 def add_error(error: str) -> None:
-    request = get_current_request()
-    _extend(request, _ERROR_MESSAGES_ATTR_NAME, [error])
+    _extend_messages(_ERROR_MESSAGES_ATTR_NAME, [error])
 
 
 def add_message(message: str) -> None:
+    _extend_messages(_NON_ERROR_MESSAGES_ATTR_NAME, [message])
+
+
+def _extend_messages(attr_name: str, messages: List[str]) -> None:
     request = get_current_request()
-    _extend(request, _NON_ERROR_MESSAGES_ATTR_NAME, [message])
-
-
-def _extend(request: HttpRequest, attr_name: str, messages: List[str]) -> None:
+    if not request:
+        # During tests etc
+        return
     if not hasattr(request, attr_name):
         setattr(request, attr_name, [])
     getattr(request, attr_name).extend(messages)
+
+
+def _get_messages(attr_name: str) -> List[str]:
+    request = get_current_request()
+    return getattr(request, attr_name, [])
 
 
 class OwldockJsonResponse(JsonResponse):
@@ -29,12 +36,11 @@ class OwldockJsonResponse(JsonResponse):
     """
 
     def __init__(self, data, errors=None, messages=None, **kwargs):
-        request = get_current_request()
-        _extend(request, _ERROR_MESSAGES_ATTR_NAME, errors or [])
-        _extend(request, _NON_ERROR_MESSAGES_ATTR_NAME, messages or [])
+        _extend_messages(_ERROR_MESSAGES_ATTR_NAME, errors or [])
+        _extend_messages(_NON_ERROR_MESSAGES_ATTR_NAME, messages or [])
 
-        errors = getattr(request, _ERROR_MESSAGES_ATTR_NAME)
-        messages = getattr(request, _NON_ERROR_MESSAGES_ATTR_NAME)
+        errors = _get_messages(_ERROR_MESSAGES_ATTR_NAME)
+        messages = _get_messages(_NON_ERROR_MESSAGES_ATTR_NAME)
 
         payload = {
             "data": data,
