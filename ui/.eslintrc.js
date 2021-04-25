d5629efd2211e5c782622435ed0ebbe8d819597d
Dan Davison <dandavison7@gmail.com>
2021-04-25 11:04:10 -0400

    eslint ignore paths

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
+  ignorePatterns: ["cypress/*", "dist/*", "node_modules/*"],
   overrides: [
     {
       files: [
