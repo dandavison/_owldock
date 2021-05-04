2cb890fe35af307a3161ff0712862d431071c9c1
Dan Davison <dandavison7@gmail.com>
2021-05-03 21:07:18 -0400

    Allow countries to be selected by bloc in the admin

---
+++ backend/immigration/admin/__init__.py
@@ -0,0 +1,132 @@
+from django.contrib import admin
+from django.forms import ModelForm, ChoiceField
+from nested_admin import (
+    NestedStackedInline,
+    NestedTabularInline,
+    NestedModelAdmin,
+)
+
+from app.models.bloc import Blocs
+from immigration.admin.bloc_choice_field import BlocChoiceFieldMixin
+from immigration.models import (
+    IssuedDocument,
+    IssuedDocumentType,
+    ProcessRuleSet,
+    ProcessStep,
+    Route,
+)
+
+
+class IssuedDocumentInline(NestedTabularInline):
+    model = IssuedDocument
+    extra = 1
+
+
+class ProcessStepAdminForm(BlocChoiceFieldMixin, ModelForm):
+    required_only_if_nationalities_bloc = ChoiceField(
+        choices=Blocs.choices() + [("", "")],
+        required=False,
+    )
+
+    _bloc_fields = [
+        ("required_only_if_nationalities", "required_only_if_nationalities_bloc"),
+    ]
+
+
+class ProcessStepInline(NestedStackedInline):
+    model = ProcessStep
+    form = ProcessStepAdminForm
+    filter_horizontal = ["required_only_if_nationalities"]
+    extra = 1
+    inlines = [IssuedDocumentInline]
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "name",
+                    "sequence_number",
+                    "estimated_min_duration_days",
+                    "estimated_max_duration_days",
+                    "applicant_can_enter_host_country_after",
+                    "applicant_can_work_in_host_country_after",
+                    "required_only_if_payroll_location",
+                    "required_only_if_duration_exceeds",
+                ]
+            },
+        ),
+        (
+            None,
+            {
+                "fields": [
+                    "required_only_if_nationalities_bloc",
+                    "required_only_if_nationalities",
+                ],
+            },
+        ),
+    ]
+
+
+@admin.register(IssuedDocumentType)
+class IssuedDocumentTypeAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    list_display_links = ["id"]
+    list_editable = ["name"]
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "host_country"]
+    list_display_links = ["id"]
+    list_editable = ["name", "host_country"]
+
+
+class ProcessRuleSetAdminForm(BlocChoiceFieldMixin, ModelForm):
+    nationalities_bloc = ChoiceField(
+        choices=Blocs.choices() + [("", "")],
+        required=False,
+    )
+    home_countries_bloc = ChoiceField(
+        choices=Blocs.choices() + [("", "")],
+        required=False,
+    )
+
+    _bloc_fields = [
+        ("nationalities", "nationalities_bloc"),
+        ("home_countries", "home_countries_bloc"),
+    ]
+
+
+@admin.register(ProcessRuleSet)
+class ProcessRuleSetAdmin(NestedModelAdmin):
+    form = ProcessRuleSetAdminForm
+    list_display = ["route"]
+    filter_horizontal = ["nationalities", "home_countries"]
+    inlines = [ProcessStepInline]
+    fieldsets = [
+        (None, {"fields": ["route"]}),
+        (
+            None,
+            {
+                "fields": ["nationalities_bloc", "nationalities"],
+            },
+        ),
+        (
+            None,
+            {
+                "fields": ["home_countries_bloc", "home_countries"],
+            },
+        ),
+        (
+            None,
+            {
+                "fields": [
+                    "contract_location",
+                    "payroll_location",
+                    "minimum_salary",
+                    "duration_min_days",
+                    "duration_max_days",
+                ]
+            },
+        ),
+    ]
---
+++ backend/immigration/admin/bloc_choice_field.py
@@ -0,0 +1,52 @@
+from typing import Any, ClassVar, Dict, List, Tuple
+
+from django.forms import ValidationError
+
+from app.models.bloc import Blocs
+
+
+class BlocChoiceFieldMixin:
+    """
+    A mixin for a form class that features a bloc ChoiceField adding country
+    selections to a Country multiple choice (m2m) field.
+    """   
+    _bloc_fields: ClassVar[List[Tuple[str, str]]] = []
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self._set_initial_bloc_values()
+
+    def _set_initial_bloc_values(self):
+        """
+        If the set of nationalities / home countries is exactly equal to one of
+        the blocs, then intialise the form with that bloc selected.
+        """
+        bloc_countries = {b.name: set(b.countries()) for b in Blocs.blocs}
+        for countries_attrname, bloc_attrname in self._bloc_fields:
+            countries = set(self.initial.get(countries_attrname, []))
+            for bloc in Blocs.blocs:
+                if bloc_countries[bloc.name] == countries:
+                    self.initial[bloc_attrname] = bloc.name
+                    continue
+
+    def clean(self) -> Dict[str, Any]:
+        """
+        Combine the bloc selector value with the country multiple choice
+        selector value to reach a single conclusion regarding which countries
+        are selected.
+
+        We currently allow one only of the two selectors to be used.
+        """
+        super().clean()
+        for countries_attrname, bloc_attrname in self._bloc_fields:
+            if self.cleaned_data.get(bloc_attrname):
+                bloc = Blocs.get_by_name(self.cleaned_data[bloc_attrname])
+                if self.cleaned_data[countries_attrname] and set(
+                    self.cleaned_data[countries_attrname]
+                ) != set(bloc.countries()):
+                    raise ValidationError(
+                        "When selecting by bloc, do not use the main country selector. "
+                        "If this is restrictive, please let the Owldock dev team know."
+                    )
+                self.cleaned_data[countries_attrname] = bloc.countries()
+        return self.cleaned_data
