3ee02e3b63976e7b744dde4dff182b9ff175d28f
Dan Davison <dandavison7@gmail.com>
2021-05-05 18:55:27 -0400

    Load europe services

---
+++ backend/immigration/data/load_europe_services.py
@@ -0,0 +1,42 @@
+import json
+from typing import Type
+
+from django.conf import settings
+from django.db.models import Model
+from django.db.transaction import atomic
+
+COUNTRY_NAMES = {"UK": "United Kingdom"}
+
+
+@atomic
+def load_europe_services(
+    Country: Type[Model],
+    ProcessRuleSet: Type[Model],
+    ProcessStep: Type[Model],
+    Route: Type[Model],
+    ServiceItem: Type[Model],
+):
+    path = (
+        settings.BASE_DIR / "immigration/data/Europe services spreadsheet 05 May.json"
+    )
+    with open(path) as fp:
+        data = json.load(fp)
+
+    for host_country_name, route_data in data.items():
+        host_country = Country.objects.get(
+            name=COUNTRY_NAMES.get(host_country_name, host_country_name)
+        )
+        for route_name, service_item_names in route_data.items():
+            if route_name == "Ancillary Services":
+                continue
+            route = Route.objects.create(name=route_name, host_country=host_country)
+            process_rule_set = ProcessRuleSet.objects.create(route=route)
+            for sequence_number, service_item_name in enumerate(service_item_names, 1):
+                process_step = ProcessStep.objects.create(
+                    process_rule_set=process_rule_set,
+                    name=service_item_name[:128],
+                    sequence_number=sequence_number,
+                )
+                ServiceItem.objects.create(
+                    process_step=process_step, description=service_item_name
+                )
