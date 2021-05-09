d5317be37befb5433231e96b41c4e7d87a75e225
Dan Davison <dandavison7@gmail.com>
2021-05-08 21:08:34 -0400

    Load countries idempotently

--- backend/app/fixtures/country.py
+++ backend/app/fixtures/country.py
@@ -10,7 +10,7 @@ def load_country_fixture() -> None:
     print("Loading countries")
     for (code, _) in django_countries.countries:
         country = django_countries.fields.Country(code)
-        Country.objects.create(
+        Country.objects.get_or_create(
             code=country.code,
             name=country.name,
             unicode_flag=country.unicode_flag,
