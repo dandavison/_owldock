10340b93f0f724519b93901157a8f1013334d6de
Dan Davison <dandavison7@gmail.com>
2021-05-09 22:22:38 -0400

    Update greece processrulesets script

--- backend/immigration/fake/create_fake_process_rulesets.py
+++ backend/immigration/fake/create_fake_process_rulesets.py
@@ -6,15 +6,27 @@ from immigration.tests import conftest
 def create_fake_process_rulesets():
     greece = Country.objects.get(name="Greece")
     conftest.greece_local_hire_article_17_rule_set.__pytest_wrapped__.obj(
-        conftest.greece_local_hire_article_17_route.__pytest_wrapped__.obj(greece)
+        greece,
+        conftest.greece_local_hire_article_17_route.__pytest_wrapped__.obj(greece),
+        conftest.greece_visa_type_D_application_step.__pytest_wrapped__.obj(greece),
+        conftest.greece_residence_permit_step.__pytest_wrapped__.obj(greece),
+        conftest.greece_biometrics_step.__pytest_wrapped__.obj(greece),
+        conftest.greece_issuance_of_residence_card_step.__pytest_wrapped__.obj(greece),
     )
     conftest.greece_eu_eea_swiss_national_registration_rule_set.__pytest_wrapped__.obj(
+        greece,
         conftest.greece_eu_eea_swiss_national_registration_route.__pytest_wrapped__.obj(
             greece
-        )
+        ),
+        conftest.greece_posted_worker_notification_step.__pytest_wrapped__.obj(greece),
+        conftest.greece_tax_registration_step.__pytest_wrapped__.obj(greece),
+        conftest.greece_eu_registration_certificate_step.__pytest_wrapped__.obj(greece),
     )
     conftest.greece_technical_assignment_article_18_route_rule_set.__pytest_wrapped__.obj(
+        greece,
         conftest.greece_technical_assignment_article_18_route.__pytest_wrapped__.obj(
             greece
-        )
+        ),
+        conftest.greece_visa_type_D_application_step.__pytest_wrapped__.obj(greece),
+        conftest.greece_posted_worker_notification_step.__pytest_wrapped__.obj(greece),
     )
