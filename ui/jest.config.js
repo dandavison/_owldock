dce535b79507fe2561c7e50e763fb55423c9a17c
Dan Davison <dandavison7@gmail.com>
2021-05-17 10:55:58 -0400

    JS tests for step rules

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  setupFiles: ["./src/__tests__/setup.js"],
+  testMatch: ["**/__tests__/*.test.[jt]s"],
 };
---
+++ ui/src/__tests__/fixtures/api--client-contact--applicants.ts
@@ -0,0 +1,334 @@
+export const response = {
+  data: [
+    {
+      id: 1,
+      uuid: "0146389e-9159-4a0d-a8a5-acb9e2ee0373",
+      user: {
+        uuid: "d0049b04-ed07-4a53-9869-63926156eccb",
+        first_name: "Tamara",
+        last_name: "Mccormick",
+        email: "tamara5-cocacola@example.com",
+      },
+      employer: {
+        uuid: "6df88634-926b-4795-9eeb-fc9748372d29",
+        name: "Coca-Cola",
+      },
+      home_country: {
+        uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+        name: "United Kingdom",
+        code: "GB",
+        currency_code: "GBP",
+        unicode_flag: "🇬🇧",
+      },
+      nationalities: [
+        {
+          uuid: "565a3297-4b1a-4576-b53b-4dc3333908de",
+          name: "Lesotho",
+          code: "LS",
+          currency_code: "LSL",
+          unicode_flag: "🇱🇸",
+        },
+        {
+          uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+          name: "United Kingdom",
+          code: "GB",
+          currency_code: "GBP",
+          unicode_flag: "🇬🇧",
+        },
+      ],
+    },
+    {
+      id: 2,
+      uuid: "4d80fd0f-7fc0-4fec-9079-6450028ae22c",
+      user: {
+        uuid: "b7a57351-cf9d-4b44-9d72-5c83c352a595",
+        first_name: "Erika",
+        last_name: "Schultz",
+        email: "erika9-cocacola@example.com",
+      },
+      employer: {
+        uuid: "6df88634-926b-4795-9eeb-fc9748372d29",
+        name: "Coca-Cola",
+      },
+      home_country: {
+        uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+        name: "United Kingdom",
+        code: "GB",
+        currency_code: "GBP",
+        unicode_flag: "🇬🇧",
+      },
+      nationalities: [
+        {
+          uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+          name: "United Kingdom",
+          code: "GB",
+          currency_code: "GBP",
+          unicode_flag: "🇬🇧",
+        },
+      ],
+    },
+    {
+      id: 3,
+      uuid: "37fb62e3-45c2-4e19-b12c-ccae94cb5bd6",
+      user: {
+        uuid: "05997497-28db-47ee-b33d-565212ffa52b",
+        first_name: "Caleb",
+        last_name: "Thomas",
+        email: "caleb5-cocacola@example.com",
+      },
+      employer: {
+        uuid: "6df88634-926b-4795-9eeb-fc9748372d29",
+        name: "Coca-Cola",
+      },
+      home_country: {
+        uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+        name: "United Kingdom",
+        code: "GB",
+        currency_code: "GBP",
+        unicode_flag: "🇬🇧",
+      },
+      nationalities: [
+        {
+          uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+          name: "United Kingdom",
+          code: "GB",
+          currency_code: "GBP",
+          unicode_flag: "🇬🇧",
+        },
+      ],
+    },
+    {
+      id: 4,
+      uuid: "cfd8c3a1-7805-4e81-a7b1-ce52242e2be6",
+      user: {
+        uuid: "f625da85-a287-403a-a78c-8193c8ebcc80",
+        first_name: "Melissa",
+        last_name: "Franco",
+        email: "melissa9-cocacola@example.com",
+      },
+      employer: {
+        uuid: "6df88634-926b-4795-9eeb-fc9748372d29",
+        name: "Coca-Cola",
+      },
+      home_country: {
+        uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+        name: "United Kingdom",
+        code: "GB",
+        currency_code: "GBP",
+        unicode_flag: "🇬🇧",
+      },
+      nationalities: [
+        {
+          uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+          name: "United Kingdom",
+          code: "GB",
+          currency_code: "GBP",
+          unicode_flag: "🇬🇧",
+        },
+      ],
+    },
+    {
+      id: 5,
+      uuid: "cb8b2152-fa9b-470c-a699-29acf152d664",
+      user: {
+        uuid: "5b3df297-deab-4599-8cd5-c55ba67a0fc5",
+        first_name: "Robert",
+        last_name: "Gordon",
+        email: "robert7-cocacola@example.com",
+      },
+      employer: {
+        uuid: "6df88634-926b-4795-9eeb-fc9748372d29",
+        name: "Coca-Cola",
+      },
+      home_country: {
+        uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+        name: "United Kingdom",
+        code: "GB",
+        currency_code: "GBP",
+        unicode_flag: "🇬🇧",
+      },
+      nationalities: [
+        {
+          uuid: "cd58569a-1f42-4ec3-8a9a-3407d2b67b1d",
+          name: "Armenia",
+          code: "AM",
+          currency_code: "AMD",
+          unicode_flag: "🇦🇲",
+        },
+        {
+          uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+          name: "United Kingdom",
+          code: "GB",
+          currency_code: "GBP",
+          unicode_flag: "🇬🇧",
+        },
+      ],
+    },
+    {
+      id: 6,
+      uuid: "f3a5017c-4caa-460a-8fd3-e130713d25ba",
+      user: {
+        uuid: "44383c2f-a9e5-472d-b47a-5e80d18b7592",
+        first_name: "Amy",
+        last_name: "Maldonado",
+        email: "amy2-cocacola@example.com",
+      },
+      employer: {
+        uuid: "6df88634-926b-4795-9eeb-fc9748372d29",
+        name: "Coca-Cola",
+      },
+      home_country: {
+        uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+        name: "United Kingdom",
+        code: "GB",
+        currency_code: "GBP",
+        unicode_flag: "🇬🇧",
+      },
+      nationalities: [
+        {
+          uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+          name: "United Kingdom",
+          code: "GB",
+          currency_code: "GBP",
+          unicode_flag: "🇬🇧",
+        },
+      ],
+    },
+    {
+      id: 7,
+      uuid: "b1d16cfa-e162-4ac4-bbfb-f948edb79b75",
+      user: {
+        uuid: "85a4eadd-0a34-4e98-aea1-c88e7919b024",
+        first_name: "Stacy",
+        last_name: "Mitchell",
+        email: "stacy4-cocacola@example.com",
+      },
+      employer: {
+        uuid: "6df88634-926b-4795-9eeb-fc9748372d29",
+        name: "Coca-Cola",
+      },
+      home_country: {
+        uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+        name: "United Kingdom",
+        code: "GB",
+        currency_code: "GBP",
+        unicode_flag: "🇬🇧",
+      },
+      nationalities: [
+        {
+          uuid: "6d005a7d-f094-4729-a0f7-b8fb2a9fac63",
+          name: "Curaçao",
+          code: "CW",
+          currency_code: null,
+          unicode_flag: "🇨🇼",
+        },
+        {
+          uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+          name: "United Kingdom",
+          code: "GB",
+          currency_code: "GBP",
+          unicode_flag: "🇬🇧",
+        },
+      ],
+    },
+    {
+      id: 8,
+      uuid: "8831e9e3-19ac-4533-9ed0-4bf82fceed1b",
+      user: {
+        uuid: "bc3b1b97-ca89-4384-a9eb-87fb7cb7f10c",
+        first_name: "Bryan-West",
+        last_name: "Jr.",
+        email: "bryan-west8-cocacola@example.com",
+      },
+      employer: {
+        uuid: "6df88634-926b-4795-9eeb-fc9748372d29",
+        name: "Coca-Cola",
+      },
+      home_country: {
+        uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+        name: "United Kingdom",
+        code: "GB",
+        currency_code: "GBP",
+        unicode_flag: "🇬🇧",
+      },
+      nationalities: [
+        {
+          uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+          name: "United Kingdom",
+          code: "GB",
+          currency_code: "GBP",
+          unicode_flag: "🇬🇧",
+        },
+      ],
+    },
+    {
+      id: 9,
+      uuid: "18fcda70-b890-4ad2-a59a-21dbf0ecf049",
+      user: {
+        uuid: "14bb177c-7880-4640-9b3e-426ce52b5b8c",
+        first_name: "Travis",
+        last_name: "Savage",
+        email: "travis4-cocacola@example.com",
+      },
+      employer: {
+        uuid: "6df88634-926b-4795-9eeb-fc9748372d29",
+        name: "Coca-Cola",
+      },
+      home_country: {
+        uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+        name: "United Kingdom",
+        code: "GB",
+        currency_code: "GBP",
+        unicode_flag: "🇬🇧",
+      },
+      nationalities: [
+        {
+          uuid: "fa8c7b62-99f6-4b80-9bb6-fe469fa0663c",
+          name: "Equatorial Guinea",
+          code: "GQ",
+          currency_code: "XAF",
+          unicode_flag: "🇬🇶",
+        },
+        {
+          uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+          name: "United Kingdom",
+          code: "GB",
+          currency_code: "GBP",
+          unicode_flag: "🇬🇧",
+        },
+      ],
+    },
+    {
+      id: 10,
+      uuid: "f10bc2b3-c141-4282-9d62-de6546434d21",
+      user: {
+        uuid: "26ed180a-4ca1-4c7d-8521-2b7cbb78a7b2",
+        first_name: "Ashley",
+        last_name: "Mcclain",
+        email: "ashley6-cocacola@example.com",
+      },
+      employer: {
+        uuid: "6df88634-926b-4795-9eeb-fc9748372d29",
+        name: "Coca-Cola",
+      },
+      home_country: {
+        uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+        name: "United Kingdom",
+        code: "GB",
+        currency_code: "GBP",
+        unicode_flag: "🇬🇧",
+      },
+      nationalities: [
+        {
+          uuid: "6456eae2-f85f-4118-9e42-c82e1a0ad53c",
+          name: "United Kingdom",
+          code: "GB",
+          currency_code: "GBP",
+          unicode_flag: "🇬🇧",
+        },
+      ],
+    },
+  ],
+  errors: [],
+  messages: [],
+};
