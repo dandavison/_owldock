ddb80ec46a8dc44a1d77a3ba52778037d4b5c9ca
Dan Davison <dandavison7@gmail.com>
2021-05-18 11:15:24 -0400

    Add max_distance and complement functionality to set decomposer

--- backend/owldock/utils/set_decomposer.py
+++ backend/owldock/utils/set_decomposer.py
@@ -1,4 +1,4 @@
-from typing import Dict, Iterable, List
+from typing import Dict, Iterable, List, Set
 
 import numpy as np
 
@@ -7,30 +7,53 @@ class SetDecomposer:
     """
     Decompose an input subset in terms of edit operations on fixed basis subsets.
 
-    >>> sd = SetDecomposer({"ab": "ab", "cd": "cd"}, "abcde")
+    If the minimum number of edit operations (Hamming distance) between the
+    input set and any basis set exceeds `max_distance` then no decomposition is
+    returned and instead the set is described by its size.
+
+    >>> sd = SetDecomposer({"ab": "ab", "cd": "cd"}, "abcde", max_distance=1.0)
     >>> sd.decompose("a")
     'ab - 1'
     >>> sd.decompose("dce")
-    'cd + 1'
+    'non-ab'
     >>> sd.decompose("abcd")
     'ab + 2'
+    >>> sd.decompose("dc")
+    'cd'
     >>> sd.decompose("dc")
     'cd'
     >>> sd.decompose("")
     ''
     >>> sd.decompose("edcab")
     '(all)'
+    >>> sd = SetDecomposer({"ab": "ab", "cd": "cd"}, "abcde", max_distance=0.3)
+    >>> sd.decompose("a")
+    'ab - 1'
+    >>> sd = SetDecomposer({"ab": "ab", "cd": "cd"}, "abcde", max_distance=0.1)
+    >>> sd.decompose("a")
+    '1'
     """
 
     _empty_description = ""
     _universe_description = "(all)"
 
-    def __init__(self, basis: Dict[str, Iterable[str]], universe: Iterable[str]):
+    def __init__(
+        self,
+        basis: Dict[str, Iterable[str]],
+        universe: Iterable[str],
+        max_distance=0.05,
+    ):
         self.universe = set(universe)
         self.size = len(set(universe))
         self.indices = {a: i for i, a in enumerate(sorted(self.universe))}
-        self.basis_names = list(basis.keys())
-        self.basis = self._make_bitvectors(list(basis.values()))
+
+        self.basis_names = list(basis.keys()) + [f"non-{name}" for name in basis.keys()]
+        basis_sets = [set(x) for x in basis.values()] + [
+            self.universe - set(x) for x in basis.values()
+        ]
+        self.basis = self._make_bitvectors(basis_sets)
+        assert self.basis.shape == (len(self.basis_names), len(self.universe))
+        self.max_distance = max_distance
 
     def _make_bitvectors(self, basis: List[Iterable[str]]) -> np.ndarray:
         bitvectors = np.zeros((len(basis), len(self.universe)), dtype=bool)
@@ -60,15 +83,25 @@ class SetDecomposer:
         n_edits = additions.sum(axis=1) + subtractions.sum(axis=1)
         closest = n_edits.argmin()
         return self._make_description(
-            self.basis_names[closest], additions[closest], subtractions[closest]
+            x,
+            self.basis_names[closest],
+            additions[closest],
+            subtractions[closest],
         )
 
     def _make_description(
-        self, base_name: str, additions: np.ndarray, subtractions: np.ndarray
+        self,
+        x: Set[str],
+        basis_set_name: str,
+        additions: np.ndarray,
+        subtractions: np.ndarray,
     ) -> str:
-        name = base_name
-        if subtractions.any():
-            name += f" - {subtractions.sum()}"
-        if additions.any():
-            name += f" + {additions.sum()}"
+        n_additions, n_subtractions = additions.sum(), subtractions.sum()
+        if n_additions + n_subtractions > len(self.universe) * self.max_distance:
+            return f"{len(x)}"
+        name = basis_set_name
+        if n_additions:
+            name += f" + {n_additions}"
+        if n_subtractions:
+            name += f" - {n_subtractions}"
         return name
