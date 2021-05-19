0932c3389850f9cb789c4ec303cb4a2851451c08
Dan Davison <dandavison7@gmail.com>
2021-05-18 20:52:22 -0400

    Add a test of admin changelist queries

--- backend/immigration/admin/tests.py
+++ backend/immigration/admin/tests.py
@@ -1,11 +1,14 @@
 from dataclasses import dataclass
 
-from django.contrib.admin.sites import AdminSite
+import pytest
 import sqlparse
+from django.contrib.admin.sites import AdminSite
 
 from immigration.admin import ProcessStepAdmin
 from immigration.models import ProcessStep
 from immigration.tests import factories
+from owldock.dev.db_utils import assert_max_queries, print_queries
+from owldock.tests.constants import TEST_PASSWORD
 
 
 class MockSuperUser:
@@ -16,6 +19,23 @@ class MockSuperUser:
 @dataclass
 class MockRequest:
     user: MockSuperUser
+
+
+def test_processruleset_admin_view_queries(
+    greece_local_hire_article_17_rule_set, admin_user_client
+):
+    id = greece_local_hire_article_17_rule_set.id
+    with assert_max_queries(20):
+        admin_user_client.get(
+            f"/admin/immigration/processruleset/{id}/change/", follow=True
+        )
+
+
+def test_processruleset_admin_list_view_queries(
+    greece_local_hire_article_17_rule_set, admin_user_client
+):
+    with assert_max_queries(20):
+        admin_user_client.get(f"/admin/immigration/processruleset/", follow=True)
 
 
 @pytest.mark.skip
